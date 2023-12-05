37d3757c65c2935805ceac4097cc8c4c626b112b
lukasz-cpu <lukasz.popek@hotmail.com>
2023-12-05 18:53:48 +0100

    add new code

--- emergency-room-service/src/main/java/eu/codification/emergencyroomservice/registration/controller/PatientRegistrationController.java
+++ emergency-room-service/src/main/java/eu/codification/emergencyroomservice/registration/controller/PatientRegistrationController.java
@@ -1,16 +1,24 @@
 package eu.codification.emergencyroomservice.registration.controller;
 
+import eu.codification.emergencyroomservice.registration.dto.PatientRegistrationDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController
+@RequestMapping(value = "/registration")
 public class PatientRegistrationController {
 
-    @GetMapping
-    public ResponseEntity<?> patientRegistration(){
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @PostMapping
+    public ResponseEntity<?> patientRegistration(@RequestBody PatientRegistrationDTO patientRegistrationDTO) {
+        log.info(patientRegistrationDTO.toString());
         return new ResponseEntity<>("Hello World!", HttpStatus.OK);
     }
 
