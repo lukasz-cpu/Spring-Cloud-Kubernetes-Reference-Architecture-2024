d84f9e9b0c2bad3f298289f56f2dfa9e232bde6e
lukasz-cpu <lukasz.popek@hotmail.com>
2023-12-12 15:43:57 +0100

    add new lines of code

--- emergency-room-service/src/main/java/eu/codification/emergencyroomservice/registration/controller/PatientRegistrationController.java
+++ emergency-room-service/src/main/java/eu/codification/emergencyroomservice/registration/controller/PatientRegistrationController.java
@@ -1,6 +1,9 @@
 package eu.codification.emergencyroomservice.registration.controller;
 
 import eu.codification.emergencyroomservice.registration.dto.PatientRegistrationDTO;
+import eu.codification.emergencyroomservice.registration.mappers.PatientRegistrationMapper;
+import eu.codification.emergencyroomservice.registration.model.PatientRegistration;
+import eu.codification.emergencyroomservice.registration.service.PatientRegistrationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -15,11 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class PatientRegistrationController {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
+    private final PatientRegistrationService patientRegistrationService;
+
+    public PatientRegistrationController(PatientRegistrationService patientRegistrationService) {
+        this.patientRegistrationService = patientRegistrationService;
+    }
 
     @PostMapping
     public ResponseEntity<?> patientRegistration(@RequestBody PatientRegistrationDTO patientRegistrationDTO) {
-        log.info(patientRegistrationDTO.toString());
-        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+        PatientRegistration patientRegistration = PatientRegistrationMapper.mapDtoToModel(patientRegistrationDTO);
+        patientRegistrationService.proceedWithRegistration(patientRegistration);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
--- emergency-room-service/src/main/java/eu/codification/emergencyroomservice/registration/service/PatientRegistrationService.java
+++ emergency-room-service/src/main/java/eu/codification/emergencyroomservice/registration/service/PatientRegistrationService.java
@@ -1,13 +1,22 @@
 package eu.codification.emergencyroomservice.registration.service;
 
+import eu.codification.emergencyroomservice.registration.model.PatientRegistration;
 import eu.codification.emergencyroomservice.registration.repository.PatientRegistrationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PatientRegistrationService {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
     private final PatientRegistrationRepository patientRegistrationRepository;
 
     public PatientRegistrationService(PatientRegistrationRepository patientRegistrationRepository) {
         this.patientRegistrationRepository = patientRegistrationRepository;
+    }
+
+    public void proceedWithRegistration(PatientRegistration patientRegistration) {
+        log.info(patientRegistration.toString());
     }
 }
