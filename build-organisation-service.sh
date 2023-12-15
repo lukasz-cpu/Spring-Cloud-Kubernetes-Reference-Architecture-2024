4519a5f07f117e9d1e890521e16df51723c06a94
lukasz-cpu <lukasz.popek@hotmail.com>
2023-12-15 13:37:51 +0100

    updated

--- build-organisation-service.sh
+++ build-organisation-service.sh
@@ -14,22 +14,12 @@
 )
 (
     cd k8s/kafka
-    kubectl apply -f kafka.yaml -n kafka
+    kubectl apply -f kafka.yaml --namespace kafka
 )
 (
-    k8s/prometheus
-    kubectl apply -f prometheus-operator-deployment.yaml -n monitoring - force-conflicts=true - server-side
-    kubectl apply -f prometheus.yaml -n monitoring
-    kubectl apply -f strimzi-pod-monitor.yaml -n monitoring
-    kubectl apply -f grafana.yaml -n monitoring
-)
-(
-   cd emergency-room-service/
-   mvn clean package -DskipTests
-)
-(
-   skaffold build
-)
-(
-   skaffold dev
+    cd k8s/prometheus
+    kubectl apply -f prometheus-operator-deployment.yaml --namespace monitoring --force-conflicts=true --server-side
+    kubectl apply -f prometheus.yaml --namespace monitoring
+    kubectl apply -f strimzi-pod-monitor.yaml --namespace monitoring
+    kubectl apply -f grafana.yaml --namespace monitoring
 )
---
+++ run_skaffold.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+(
+   cd emergency-room-service/
+   mvn clean package -DskipTests
+)
+(
+   skaffold build
+)
+(
+   skaffold dev
+)
