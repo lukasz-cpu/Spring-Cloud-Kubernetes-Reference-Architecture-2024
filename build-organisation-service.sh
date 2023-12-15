95af0e7662b684de25408874267a9a474825f1d5
lukasz-cpu <lukasz.popek@hotmail.com>
2023-12-15 10:00:59 +0100

    updated deployment

--- build-organisation-service.sh
+++ build-organisation-service.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 (
+  kubectl delete namespace kafka
+  kubectl delete namespace spring-cloud-app
+)
+(
    cd emergency-room-service/
    mvn clean package -DskipTests
 )
 (
    skaffold build
+)
+(
+   kubectl create namespace kafka
+   kubectl create namespace spring-cloud-app
 )
 (
    skaffold dev
