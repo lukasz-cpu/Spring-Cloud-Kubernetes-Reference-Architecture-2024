0fbf7b23406a98ff5b71c0c8e1e017c1904b2ae4
lukasz-cpu <lukasz.popek@hotmail.com>
2023-12-15 14:00:37 +0100

    Update provision_kafka.sh

--- provision_kafka.sh
+++ provision_kafka.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 (
    kubectl delete ns kafka
-   kubectl delete namespace monitoring
    kubectl create ns kafka
-   kubectl create namespace monitoring
 )
 (
-   kubectl create ns kafka
-   kubectl create namespace monitoring
    helm repo add strimzi https://strimzi.io/charts/
    helm repo update
    helm install strimzi strimzi/strimzi-kafka-operator --namespace kafka
