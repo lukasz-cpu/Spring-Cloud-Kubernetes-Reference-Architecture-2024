ccb00a041f804f80a1db6084d5734aeb00ce9d64
lukasz-cpu <lukasz.popek@hotmail.com>
2023-12-15 13:26:52 +0100

    updated files

--- build-organisation-service.sh
+++ build-organisation-service.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 (
+   kubectl delete ns kafka
+   kubectl delete namespace monitoring
+   kubectl create ns kafka
+   kubectl create namespace monitoring
+)
+(
+   kubectl create ns kafka
+   kubectl create namespace monitoring
+   helm repo add strimzi https://strimzi.io/charts/
+   helm repo update
+   helm install strimzi strimzi/strimzi-kafka-operator --namespace kafka
+)
+(
+    cd k8s/kafka
+    kubectl apply -f kafka.yaml -n kafka
+)
+(
+    k8s/prometheus
+    kubectl apply -f prometheus-operator-deployment.yaml -n monitoring - force-conflicts=true - server-side
+    kubectl apply -f prometheus.yaml -n monitoring
+    kubectl apply -f strimzi-pod-monitor.yaml -n monitoring
+    kubectl apply -f grafana.yaml -n monitoring
+)
+(
    cd emergency-room-service/
    mvn clean package -DskipTests
 )
