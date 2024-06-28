0726a6e7c91f3ca487344e5e6cb1ac5b33a0e2f6
lukasz-cpu <lukasz.popek@hotmail.com>
2024-06-28 18:08:22 +0200

    wait for

--- run_skaffold.sh
+++ run_skaffold.sh
@@ -9,6 +9,12 @@ mvn clean package -DskipTests || { echo "Budowanie Maven nie powiodło się"; ex
 # Powrót do pierwotnego katalogu
 cd - || { echo "Nie udało się wrócić do pierwotnego katalogu"; exit 1; }
 
+cd config-server/ || { echo "Nie udało się przejść do katalogu emergency-room-service"; exit 1; }
+
+mvn clean package -DskipTests || { echo "Budowanie Maven nie powiodło się"; exit 1; }
+
+cd - || { echo "Nie udało się wrócić do pierwotnego katalogu"; exit 1; }
+
 # Budowanie projektu za pomocą Skaffold
 skaffold build || { echo "Budowanie Skaffold nie powiodło się"; exit 1; }
 
