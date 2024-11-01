389a5e495804c05bb8e5ec32fe68d368c13491cb
lukasz-cpu <lukasz.popek@hotmail.com>
2024-11-01 21:54:28 +0100

    Update PatientRegistrationController.java

--- emergency-room-service/src/main/java/eu/codification/emergencyroomservice/registration/controller/PatientRegistrationController.java
+++ emergency-room-service/src/main/java/eu/codification/emergencyroomservice/registration/controller/PatientRegistrationController.java
@@ -9,16 +9,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 @RequestMapping(value = "/registration")
+
 public class PatientRegistrationController {
     private final Logger log = LoggerFactory.getLogger(getClass());
     private final PatientRegistrationService patientRegistrationService;
+
+    @Value("${my.custom.property}")
+    private String customProperty;
 
     public PatientRegistrationController(PatientRegistrationService patientRegistrationService) {
         this.patientRegistrationService = patientRegistrationService;
@@ -31,4 +33,8 @@ public class PatientRegistrationController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @GetMapping("/custom-property")
+    public ResponseEntity<String> getCustomProperty() {
+        return new ResponseEntity<>(customProperty, HttpStatus.OK);
+    }
 }
