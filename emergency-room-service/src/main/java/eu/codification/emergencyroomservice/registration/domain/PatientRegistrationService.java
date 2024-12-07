56cb4c6116ddeca74a43a613cfb43f6b9ffc0d49
lukasz-cpu <lukasz.popek@hotmail.com>
2024-12-07 22:07:47 +0100

    Update PatientRegistrationService.java

--- emergency-room-service/src/main/java/eu/codification/emergencyroomservice/registration/domain/PatientRegistrationService.java
+++ emergency-room-service/src/main/java/eu/codification/emergencyroomservice/registration/domain/PatientRegistrationService.java
@@ -2,9 +2,11 @@ package eu.codification.emergencyroomservice.registration.domain;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.client.ClientSession;
 import eu.codification.emergencyroomservice.registration.infrastructure.PatientRegistrationEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +16,9 @@ public class PatientRegistrationService {
     private final Logger log = LoggerFactory.getLogger(getClass());
     private final PatientRegistrationRepository patientRegistrationRepository;
     private final KafkaTemplate<String, String> kafkaTemplate;
+    @Autowired
+    ClientSession clientSession;
+
 
     private ObjectMapper objectMapper;
 
