61af8b3bdf4cfcb43868a1a7251ec14a63fa08fc
lukasz-cpu <lukasz.popek@hotmail.com>
2024-12-07 21:55:52 +0100

    asd

--- k8s-pre-setup/argo/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl port-forward svc/argocd-server -n argocd 8080:443 &
-ARGOCD_PASSWORD=$(argocd admin initial-password -n argocd)
-echo "Hasło administracyjne ArgoCD: $ARGOCD_PASSWORD"
-kubectl create namespace spring-cloud
-kubectl apply -f spring-cloud-app.yaml
---
+++ readme.md
@@ -0,0 +1,6 @@
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl port-forward svc/argocd-server -n argocd 8080:443
+argocd admin initial-password -n argocd
+kubectl create namespace spring-cloud
+kubectl apply -f spring-cloud-app.yaml
