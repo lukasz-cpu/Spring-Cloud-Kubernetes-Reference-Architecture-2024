d25a1614eef8000741e5c41ff3fd20427ab0c05e
lukasz-cpu <lukasz.popek@hotmail.com>
2024-12-08 23:31:22 +0100

    dodac by przyjmowalo wiadomosci

---
+++ patients-analyzer/src/main/java/eu/codification/patientsanalyzer/PatientsAnalyzer.java
@@ -0,0 +1,20 @@
+package eu.codification.patientsanalyzer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaHandler;
+
+@SpringBootApplication
+public class PatientsAnalyzer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientsAnalyzer.class, args);
+    }
+
+    @KafkaHandler
+    public void process(ConsumerRecord<String, String> record) {
+
+    }
+
+}
