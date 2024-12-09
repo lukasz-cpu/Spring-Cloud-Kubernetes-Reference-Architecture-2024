a934745c348d7fc03963fd398bed31be4a2ae9f1
lukasz-cpu <lukasz.popek@hotmail.com>
2024-12-09 21:06:50 +0100

    Update KafkaConfiguration.java

--- emergency-room-service/src/main/java/eu/codification/emergencyroomservice/registration/infrastructure/KafkaConfiguration.java
+++ emergency-room-service/src/main/java/eu/codification/emergencyroomservice/registration/infrastructure/KafkaConfiguration.java
@@ -29,7 +29,7 @@ public class KafkaConfiguration {
     @Bean
     public Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-service:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, address);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return props;
