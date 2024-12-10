9a7ecc4923a5a5d7a34441b02ba260b5eaf6ff20
lukasz-cpu <lukasz.popek@hotmail.com>
2024-12-10 18:29:30 +0100

    Update OutboxProcessorService.java

--- emergency-room-service/src/main/java/eu/codification/emergencyroomservice/registration/domain/OutboxProcessorService.java
+++ emergency-room-service/src/main/java/eu/codification/emergencyroomservice/registration/domain/OutboxProcessorService.java
@@ -57,7 +57,7 @@ public class OutboxProcessorService {
     }
   }
 
-    @KafkaListener(topics = "${kafka.patient-registration.topic}")
+    @KafkaListener(topics = "${kafka.patient-registration.topic}", groupId = "default-consumer-group")
     public void listenToPatientRegistrationTopic(String message) {
         log.info("Received message from Kafka topic '{}': {}", patientRegistrationOutBoxTopic, message);
     }
